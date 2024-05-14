e68bcbccd8c62393533680c6a0c2b49d375c0de3
Mehedi Hasan Nayeem <mehedinayeem@Mehedis-MacBook-Air.local>
2024-05-14 13:51:49 +0600

    Implemented Google authentication feature

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { GoogleAuthProvider, getAuth, onAuthStateChanged, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js"
 import { getDatabase, onValue, push as pushRef, ref, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
-    databaseURL: "https://add-to-cart-95318-default-rtdb.asia-southeast1.firebasedatabase.app/"
+    databaseURL: "https://add-to-cart-95318-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    apiKey: "AIzaSyBztv4u7BKg8tReyNaZu5EgPZnPVQlEK98",
+    authDomain: "add-to-cart-95318.firebaseapp.com",
 }
 
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
+const auth = getAuth(app)
+const provider = new GoogleAuthProvider()
 const shoppingListInDB = ref(database, "shoppingList")
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 const shoppingListEl = document.getElementById("shopping-list")
+const viewLoggedIn = document.getElementById("view-logged-in")
+const viewLoggedOut = document.getElementById("view-logged-out")
+const continueWithGooglePopUpEL = document.getElementById("google-btn")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
@@ -19,6 +27,16 @@ addButtonEl.addEventListener("click", function() {
     pushRef(shoppingListInDB, inputValue)
     
     clearInputFieldEl()
+})
+
+continueWithGooglePopUpEL.addEventListener("click", ()=>{
+    signInWithPopup(auth, provider)
+        .then((result)=>{
+            console.log("Sign in With Google", result)
+        })
+        .then((error)=>{
+            console.log(error.code)
+        })
 })
 
 onValue(shoppingListInDB, function(snapshot) {
@@ -37,6 +55,16 @@ onValue(shoppingListInDB, function(snapshot) {
     }
     } else{
         shoppingListEl.textContent = 'No items here... yet'
+    }
+})
+
+onAuthStateChanged(auth, function(user){
+    if(user){
+        viewLoggedIn.style.display = "block"
+        viewLoggedOut.style.display = "none"
+    }else{
+        viewLoggedIn.style.display = "none"
+        viewLoggedOut.style.display = "block"
     }
 })
 
